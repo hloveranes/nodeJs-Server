c564ce91da654ba86a853a6a827feebba0113339
devy-tech <hloveranes1@gmail.com>
2020-08-02 15:02:00 +0800

    adding validation

---
+++ validation.js
@@ -0,0 +1,8 @@
+// validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required(),
+    password: Joi.string().min(6).required()
+})
