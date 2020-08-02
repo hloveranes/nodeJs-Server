4e8f4525f54d7a1a5bf44b49d4456745015844a9
devy-tech <hloveranes1@gmail.com>
2020-08-02 17:38:00 +0800

    hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,37 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const { registerValidation, loginValidation } = require('../validation.js');
 
-router.post('/register', async (req, res) => {
+// const value = await Promise.all([schema.validateAsync(req.body)]);
 
-    // validate the data before creating a user
+router.get('/all', async (req, res) => {
     try {
-
-        const value = await Promise.all([schema.validateAsync(req.body)]);
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
-        user.save()
-            .then(savedUser => {
-                res.status(200).json({ data: savedUser });
-            })
-            .catch(err => console.log(err));
-    } catch (err) {
-        res.status(400).send(err.details[0].message);
+        const allUser = await User.find();
+        res.status(200).json({ data: allUser })
+    } catch (error) {
+        res.status(500).json({ errMsg: error })
     }
 })
 
+router.post('/register', (req, res) => {
+    // validate the data before creating a user
+    registerValidation(req.body).then(something => {
+        // const emailExist = await User.findOne({ email: req.body.email });
+        User.findOne({ email: req.body.email }).then(emailExist => {
+            if (emailExist) return res.status(400).json(`Email already exists ${emailExist.email}`);
+            // const salt = await bcrypt.genSalt(10);
+            bcrypt.genSalt(10).then(salt => {
+                bcrypt.hash(req.body.password, salt).then(hashedPassword => {
+                    const user = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: hashedPassword
+                    });
+                    user.save().then(savedUser => { res.status(200).json({ data: savedUser }) }).catch(err => res.status(500).json(err));
+                }).catch(err => { res.status(500).json(err); })
+            }).catch(err => { res.status(500).json(err); })
+        }).catch(err => { res.status(500).json(err); })
+    }).catch(err => { res.status(400).json(err.details[0].message); })
+});
 module.exports = router;
--- validation.js
+++ validation.js
@@ -1,8 +1,25 @@
 // validation
 const Joi = require('@hapi/joi');
 
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required(),
-    password: Joi.string().min(6).required()
-})
+
+// const registerValidation = async (data) => {
+const registerValidation = data => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required(),
+        password: Joi.string().min(6).required()
+    })
+    // return schema.validateAsync(data);
+    return schema.validateAsync(data);
+}
+
+const loginValidation = async data => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required(),
+        password: Joi.string().min(6).required()
+    })
+    return await Promise.all([schema.validateAsync(data)]);
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
